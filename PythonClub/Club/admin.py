ad34befe817d2e944e772f80de1b21ad0e5e36b9
olverj <103541486+olverj@users.noreply.github.com>
2022-04-25 18:17:22 -0700

    Add files via upload

--- PythonClub/Club/admin.py
+++ PythonClub/Club/admin.py
@@ -1,3 +1,14 @@
+<<<<<<< HEAD
+from django.contrib import admin
+from .models import Meeting, MeetingMinutes, Resource, Event
+# Register your models here.
+
+admin.site.register(Meeting)
+admin.site.register(MeetingMinutes)
+admin.site.register(Resource)
+admin.site.register(Event)
+=======
 from django.contrib import admin
 
 # Register your models here.
+>>>>>>> a027899785214d5d4a341aaeb940aa9cc8de7838
